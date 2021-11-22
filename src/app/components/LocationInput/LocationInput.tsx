69fae06d2c021a8cf83757bfe5b85b6811205f58
Julia Loeschel <juloeschel@gmail.com>
2021-11-22 16:16:04 +0100

    Setup HTML and Styling

--- src/app/components/LocationInput/LocationInput.tsx
+++ src/app/components/LocationInput/LocationInput.tsx
@@ -1,5 +1,44 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export default function LocationInput(): JSX.Element {
-  return <p>Hallo</p>
+  return (
+    <>
+      <Container>
+        <Form>
+          <Label>
+            Location 1
+            <input type="text" />
+          </Label>
+
+          <Label>
+            Location 2
+            <input type="text" />
+          </Label>
+        </Form>
+        <Button>find middle</Button>
+      </Container>
+    </>
+  )
 }
+
+const Container = styled.div`
+  display: grid;
+  padding: 50px 20px 20px 20px;
+  gap: 20px;
+`
+
+const Form = styled.form`
+  display: grid;
+
+  gap: 20px;
+`
+const Label = styled.label`
+  display: grid;
+  gap: 5px;
+`
+const Button = styled.button`
+  padding: 10px 20px;
+  max-width: 170px;
+  justify-self: center;
+`
