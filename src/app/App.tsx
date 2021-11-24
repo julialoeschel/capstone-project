dca30d18d852c5da2f088284b8e98a518e8a084b
Julia Loeschel <juloeschel@gmail.com>
2021-11-24 12:44:44 +0100

    clean up commented out code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import LocationInput from './components/LocationInput/LocationInput'
 import Map from './components/Map/Map'
 
-//import { BrowserRouter } from 'react-router-dom'
-
 function App(): JSX.Element {
   return (
     <>
