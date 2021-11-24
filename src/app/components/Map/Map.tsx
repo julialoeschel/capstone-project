4ffa6b807cb596a4a53c6d3314f2309e6f08ba9f
Julia Loeschel <juloeschel@gmail.com>
2021-11-24 15:43:42 +0100

    Add geocoder funcrionality

--- src/app/components/Map/Map.tsx
+++ src/app/components/Map/Map.tsx
@@ -1,35 +1,51 @@
 import 'mapbox-gl/dist/mapbox-gl.css'
 import React, { useEffect, useRef, useState } from 'react'
+import type { Map } from 'mapbox-gl'
 import mapboxgl from 'mapbox-gl' // or "const mapboxgl = require('mapbox-gl');"
 import styled from 'styled-components'
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoianVsb2UiLCJhIjoiY2t3YzZrZHVmMGhvajJ2bjJjZWIwZGc4MCJ9.7wNghP5-UkXaVC8CuaxH6A'
+if (process.env.MAPBOX_ACCESKEY) {
+  mapboxgl.accessToken = process.env.MAPBOX_ACCESKEY
+}
 
-export default function Map(): JSX.Element {
-  const mapContainer = useRef(null)
-  const map = useRef<HTMLInputElement | null>(null)
-  const [lng, setLng] = useState(9.96)
-  const [lat, setLat] = useState(53.55)
-  const [zoom, setZoom] = useState(8)
+export default function MapBox(): JSX.Element {
+  const mapContainer = useRef<HTMLDivElement | null>(null)
+  const map = useRef<null | Map>(null)
+  const [lng, setLng] = useState<number>(9.96)
+  const [lat, setLat] = useState<number>(53.55)
+  const [zoom, setZoom] = useState<number>(8)
+
+  const testMarker = new mapboxgl.Marker({ color: 'hotpink' })
 
   useEffect(() => {
     if (map && map.current) return // initialize map only once
     map.current = new mapboxgl.Map({
-      container: mapContainer.current,
+      container: mapContainer.current as HTMLElement,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [lng, lat],
       zoom: zoom,
     })
-  })
-  useEffect(() => {
-    if (!map.current) return // wait for map to initialize
-    map.current.on('move', () => {
-      setLng(map.current?.getCenter().lng.toFixed(4))
-      setLat(map.current?.getCenter().lat.toFixed(4))
-      setZoom(map.current?.getZoom().toFixed(2))
+
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      marker: testMarker,
+      mapboxgl: map.current,
     })
-  })
+
+    map.current.addControl(geocoder)
+  }, [])
+  useEffect(() => {
+    map.current?.on('move', () => {
+      if (map.current) {
+        setLng(map.current.getCenter().lng)
+        setLat(map.current.getCenter().lat)
+        setZoom(map.current.getZoom())
+      }
+    })
+    // wait for map to initialize
+  }, [])
 
   return (
     <div>
@@ -43,8 +59,8 @@ export default function Map(): JSX.Element {
 
 const MapContainer = styled.div`
   height: 200px;
-  width: 200px;
-  margin-left: 20px;
+  width: 100%;
+  position: relative;
 `
 const SidebarContainer = styled.div`
   background-color: rgba(35, 55, 75, 0.9);
