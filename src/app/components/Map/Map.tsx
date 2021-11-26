86bd45204e14bb916a98bbe2eea06111e1ca0389
Julia Loeschel <juloeschel@gmail.com>
2021-11-26 16:14:20 +0100

    Update Map display

--- src/app/components/Map/Map.tsx
+++ src/app/components/Map/Map.tsx
@@ -3,9 +3,8 @@ import React, { useEffect, useRef, useState } from 'react'
 import type { Map } from 'mapbox-gl'
 import mapboxgl from 'mapbox-gl'
 import styled from 'styled-components'
-import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
 import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
-
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
 
 if (typeof import.meta.env.VITE_MAPBOX_ACCESSKEY === 'string') {
@@ -17,78 +16,84 @@ if (typeof import.meta.env.VITE_MAPBOX_ACCESSKEY === 'string') {
 export default function MapBox(): JSX.Element {
   const mapContainer = useRef<HTMLDivElement | null>(null)
   const map = useRef<null | Map>(null)
-  const [lng, setLng] = useState<number>(9.96)
-  const [lat, setLat] = useState<number>(53.55)
-  const [zoom, setZoom] = useState<number>(8)
   const [distance, setDistance] = useState<number>(0)
+  const [location, setLocation] = useState([])
+  //const [geocoder, setGeocoder] = useState<MapboxGeocoder>()
 
+  // initialize map only once
   useEffect(() => {
-    if (map && map.current) return // initialize map only once
+    if (map && map.current) return
     map.current = new mapboxgl.Map({
       container: mapContainer.current as HTMLElement,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom,
+      style: 'mapbox://styles/mapbox/outdoors-v11',
+      center: [10, 53.55],
+      zoom: 8,
     })
-
-    async function getRoute(start: number[], end: number[]) {
-      const query = await fetch(
-        `https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
-        { method: 'GET' }
-      )
-      const json = await query.json()
-      const data = json.routes[0]
-      console.log(data)
-      console.log(data.distance)
-      setDistance(data.distance)
-    }
-    getRoute([9.993682, 53.551086], [8.801694, 53.079296])
-
-    const mapDirections = new MapboxDirections({
-      accessToken: mapboxgl.accessToken,
-    })
-    map.current.addControl(mapDirections, 'top-left')
+    //add MapControl
     const mapControl = new mapboxgl.NavigationControl()
     map.current.addControl(mapControl)
+
+    //initiallize Geocoder and set to Div
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+    })
+    geocoder.addTo('#locationInput')
+    geocoder.on('result', function (results) {
+      setLocation(results.result.center)
+      console.log('newest location = ', results.result.center)
+    })
   }, [])
 
-  useEffect(() => {
-    map.current?.on('move', () => {
-      if (map.current) {
-        setLng(map.current.getCenter().lng)
-        setLat(map.current.getCenter().lat)
-        setZoom(map.current.getZoom())
-      }
-    })
-    // wait for map to initialize
-  }, [])
+  // get Distance from API to [7.43861, 46.95083] (its Bremen)
+  async function getRoute(start: number[], end: number[]) {
+    const query = await fetch(
+      `https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+      { method: 'GET' }
+    )
+    const json = await query.json()
+    const data = json.routes[0]
+    console.log(data.distance)
+    setDistance(data.distance)
+  }
+
+  function handleclick() {
+    getRoute(location, [7.43861, 46.95083])
+  }
 
   return (
-    <div>
+    <Container>
       <span>Distance: {distance} m </span>
+      <LocationInput id="locationInput" />
+      <Button
+        onClick={() => {
+          handleclick()
+        }}
+      >
+        Get Distance to Bremen
+      </Button>
       <MapContainer ref={mapContainer} className="map-container" />
-      <SidebarContainer className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </SidebarContainer>
-    </div>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: grid;
+  gap: 5px;
+`
 
 const MapContainer = styled.div`
   height: 100vh;
   width: 90vw;
   position: relative;
   margin: auto;
+  margin-top: 30px;
 `
-const SidebarContainer = styled.div`
-  display: inline-block;
-  background-color: rgba(35, 55, 75, 0.9);
-  padding: 0 5px;
-  color: #fff;
-  font-family: monospace;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  margin: 12px;
-  border-radius: 4px;
+
+const LocationInput = styled.div`
+  background-color: #e6e4e4;
+  padding: 20px;
+  height: 80px;
+`
+const Button = styled.button`
+  padding: 10px;
 `
