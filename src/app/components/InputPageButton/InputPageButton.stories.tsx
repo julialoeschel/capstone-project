b4c5832a64c8ee402ab33bedf69e8836987269c0
Julia Loeschel <juloeschel@gmail.com>
2021-11-28 18:02:43 +0100

    Create Story

---
+++ src/app/components/InputPageButton/InputPageButton.stories.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import InputPageButton from './InputPageButton'
+
+// test if storybook works
+export default {
+  title: 'Component/InputPageButton',
+  component: 'InputPageButton',
+}
+
+export const setLocation = (): JSX.Element => (
+  <InputPageButton
+    onClick={function (): void {
+      throw new Error('Function not implemented.')
+    }}
+  >
+    set location
+  </InputPageButton>
+)
+
+export const clear = (): JSX.Element => (
+  <InputPageButton
+    onClick={function (): void {
+      throw new Error('Function not implemented.')
+    }}
+  >
+    clear all inputs
+  </InputPageButton>
+)
