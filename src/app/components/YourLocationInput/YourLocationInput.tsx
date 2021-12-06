ae03143c6ccd372e58c6e5162d0bedabc5905b6d
Julia Loeschel <juloeschel@gmail.com>
2021-12-06 22:45:05 +0100

    Constructed Arrows

--- src/app/components/YourLocationInput/YourLocationInput.tsx
+++ src/app/components/YourLocationInput/YourLocationInput.tsx
@@ -13,7 +13,45 @@ export default function YourLocationInput({
   return (
     <Container>
       <Text>Your locations are set to:</Text>
+      <svg
+        width="60"
+        height="50"
+        viewBox="0 0 117 93"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M13.4688 12.8667C34.6989 85.9508 87.089 74.826 110.63 60.1281"
+          stroke="#FFE4A0"
+          stroke-width="5"
+        />
+        <path
+          d="M95 77C109 64.2 98.1667 60.3333 91 60H111L95 77Z"
+          stroke="#FFE4A0"
+          stroke-width="5"
+        />
+      </svg>
+
       <Location1>{locationName1}</Location1>
+
+      <svg
+        width="60"
+        height="50"
+        viewBox="0 0 117 93"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M13.4688 12.8667C34.6989 85.9508 87.089 74.826 110.63 60.1281"
+          stroke="#FFE4A0"
+          stroke-width="5"
+        />
+        <path
+          d="M95 77C109 64.2 98.1667 60.3333 91 60H111L95 77Z"
+          stroke="#FFE4A0"
+          stroke-width="5"
+        />
+      </svg>
       <Location2>{locationName2}</Location2>
     </Container>
   )
