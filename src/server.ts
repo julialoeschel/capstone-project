bb852cbf47c10f0d0f5fb16bd028e3d25c587aa2
Julia Loeschel <juloeschel@gmail.com>
2021-12-08 15:55:16 +0100

    Implement server request to fouresquare api

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,32 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import express from 'express'
+import fetch from 'node-fetch'
+
+if (!process.env.KEY_FOURESQUARE) throw new Error('no KEY-Fouresquare provided')
 
 const app = express()
 const port = process.env.PORT || 3001
 
 app.use(express.json())
 
-app.get('/api/places/:long/:lat', async (req, res) => {
-  const places = fetch('foursquare')
+app.get('/api/places:long//:lat/:radius', async (req, res) => {
+  const { lat, long, radius } = req.params
+
+  const options = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      Authorization: `${process.env.KEY_FOURESQUARE}`,
+    },
+  }
+
+  const response = await fetch(
+    `https://api.foursquare.com/v3/places/search?ll=${long}%2C${lat}&radius=${radius}&categories=19000&limit=25`,
+    options
+  )
+  const places = await response.json()
+
   res.send(places)
 })
 
